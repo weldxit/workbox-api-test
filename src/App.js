fa012e97d98f58d7ba5f3c6649a0d4321593e410
weldxit <subhransu.weldxit@gmail.com>
2023-12-15 19:12:00 +0530

    workbox

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
 function App() {
+useEffect(()=>{
+const fetchPost = async () =>{
+  const response = axios.get('https://jsonplaceholder.org/posts')
+  const data = (await response).data
+  console.log(data)
+}
+fetchPost()
+},[])
   return (
     <div className="App">
       <header className="App-header">
